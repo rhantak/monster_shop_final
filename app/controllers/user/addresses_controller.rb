6978359fc6cd5a6a485b11249973d3abdc90c95e
Ryan Hantak <rhantak@gmail.com>
2019-11-05 14:57:29 -0700

    Add route, controller actions, and view for address creation

--- app/controllers/user/addresses_controller.rb
+++ app/controllers/user/addresses_controller.rb
@@ -8,4 +8,24 @@ class User::AddressesController < ApplicationController
   def show
     @address = Address.find(params[:address_id])
   end
+
+  def new
+
+  end
+
+  def create
+    address = current_user.addresses.new(address_params)
+    if address.save
+      flash[:success] = "New address has been created!"
+      redirect_to '/profile/addresses'
+    else
+      flash.now[:error] = address.errors.full_messages.to_sentence
+      render :new
+    end
+  end
+
+  private
+    def address_params
+      params.permit(:nickname, :name, :street_address, :city, :state, :zip)
+    end
 end
