dbdbe549fd3902ccf4694198d44536ca094220f0
Ryan Hantak <rhantak@gmail.com>
2019-11-05 22:32:36 -0700

    Add test for address being saved upon user registration

--- spec/features/users/registration_spec.rb
+++ spec/features/users/registration_spec.rb
@@ -10,17 +10,33 @@ RSpec.describe 'User Registration' do
       expect(current_path).to eq(registration_path)
     end
 
-    it 'I can register as a user' do
+    it 'I can register as a user and my address is saved' do
       visit registration_path
 
       fill_in 'Name', with: 'Megan'
       fill_in 'Email', with: 'megan@example.com'
       fill_in 'Password', with: 'securepassword'
       fill_in 'Password confirmation', with: 'securepassword'
+      fill_in :user_address_nickname, with: 'My Home'
+      fill_in :user_address_name, with: 'Megan'
+      fill_in :user_address_street_address, with: '123 A Street'
+      fill_in :user_address_city, with: 'Denver'
+      fill_in :user_address_state, with: 'CO'
+      fill_in :user_address_zip, with: '80202'
       click_button 'Register'
 
       expect(current_path).to eq(profile_path)
       expect(page).to have_content('Welcome, Megan!')
+
+      click_link 'My Addresses'
+
+        expect(page).to have_content("My Home")
+        expect(page).to have_content("Megan")
+        expect(page).to have_content("123 A Street")
+        expect(page).to have_content('Denver')
+        expect(page).to have_content('CO')
+        expect(page).to have_content('80202')
+
     end
 
     describe 'I can not register as a user if' do
